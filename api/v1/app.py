24e18d448f28219a0ef006dced871085be9c2603
amlaksil <silamlakdesye@gmail.com>
2023-08-01 23:32:02 +0300

    Update all

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """
 Setup flask application
 """
-from api.v1.views import app_views
+from api.v1.views.index import app_views
 from flask import Flask
 from models import storage
 from os import getenv
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 """Create route"""
-from api.v1.views import app_views
+#from api.v1.views import app_views
+from flask import Blueprint
 from flask import jsonify
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns Json object"""
     return jsonify({"status": "OK"})
-
-'''
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-	"""Returns the number of instance """
-	return jsonify(jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-users=storage.count("User")))
-'''
